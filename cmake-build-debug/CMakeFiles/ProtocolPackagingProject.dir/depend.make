b4f25307cce77b68a977dfe209d8bdf6e4445d20
SirHao <2775153204@qq.com>
2019-10-31 10:15:10 -0400

    aproject

--- cmake-build-debug/CMakeFiles/ProtocolPackagingProject.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ProtocolPackagingProject.
-# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/network_pro.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/network_pro.dir/main.c.o"
+  "CMakeFiles/network_pro.dir/application.c.o"
+  "CMakeFiles/network_pro.dir/package.c.o"
+  "CMakeFiles/network_pro.dir/stream.c.o"
+  "network_pro.pdb"
+  "network_pro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/network_pro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
